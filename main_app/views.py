7ba2f70ca89e89dfe7066a785c482920fbb5bb2f
Judy <kimjudyh@gmail.com>
2020-05-14 14:48:23 -0700

    added language index url path and view

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# TODO: change Subject to Language
+from .models import Subject, Student, Teacher
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+
+# ==== STUDENT VIEWS
+
+
+# ==== TEACHER VIEWS
+
+
+# ==== LESSON VIEWS
+
+
+# ==== LANGUAGE VIEWS
+# Language Index View
+def language_index(request):
+    # get all languages from database
+    # TODO: change Subject to Language
+    languages = Subject.objects.all()
+    template = 'languages/index.html'
+    context = {
+        'languages': languages,
+    }
+    # render template
+    return render(request, template, context)
