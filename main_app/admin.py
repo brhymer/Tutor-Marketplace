47c36cb4ce69f411d488428c0f615d148a20cf7c
Brhymer <leiqien28@hotmail.com>
2020-05-14 11:30:21 -0700

    adds Student and Teacher data models, admin.site.register statements

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Teacher, Student, Lesson, Subject
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Lesson)
+admin.site.register(Subject)
--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+LEVEL = (
+  ('B', 'Beginner'),
+  ('I', 'Intermediate'),
+  ('A', 'Advanced'),
+)
+
+# Data Models
+
+class Teacher(models.Model):
+    full_name = models.CharField(max_length =100)
+    student = models.ManyToManyField(Student)
+    subject = models.ManyToManyField(Subject)
+    lesson = models.ManyToManyField(Lesson)
+    level = models.CharField(
+    'Level',
+    max_length=1,
+    choices=LEVEL,
+    default=LEVEL[0][0]
+  )
+    def __str__(self):
+        return self.name
 
 
-# Models
+class Student(models.Model):
+    full_name = models.CharField(max_length =100)
+    teacher = models.ManyToManyField(Teacher)
+    subject = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.name
 
 class Lesson(models.Model):
     name = models.CharField(max_length =50)
@@ -24,6 +51,7 @@ class Subject(models.Model):
     teacher = models.ForeignKey(Teacher)
 
     def __str__(self):
-        
+        return self.name
+
 
 
