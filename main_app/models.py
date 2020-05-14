250b61b37f925729864786f7898bbd92c9e0882a
Brhymer <leiqien28@hotmail.com>
2020-05-14 11:20:46 -0700

    adds Lesson and Subject data models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+# Models
+
+class Lesson(models.Model):
+    name = models.CharField(max_length =50)
+    # The below: ForeignKey or ManyToManyField?
+    teacher = models.ManyToManyField(Teacher)
+    student = models.ManyToManyField(Student)
+    subject = models.ManyToManyField(Subject)
+    description = models.TextField(max_length = 250)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length =50)
+    teacher = models.ForeignKey(Teacher)
+
+    def __str__(self):
+        
+
+
